2b7579917755b5c70a7faf50561990d360a19b76
Sushant <unknownsushant.0042@gmail.com>
2026-01-20 22:29:32 +0530

    cards done

--- Myntra_clone/scripts/index.js
+++ Myntra_clone/scripts/index.js
@@ -1,32 +1,24 @@
 let itemsContainerElement = document.querySelector('.items-container');
 
-let item = {
-  item_image : '1.jpg',
-  rating: {
-    stars: 4.5,
-    reviews : 2000,
-  },
-  company_name: 'Carlton London',
-  item_name: 'Rhodium-Plated CZ Floral Studs',
-  current_price:'606',
-  original_price: '1045',
-  discount: '42'
-}; // mking components generic
-
-itemsContainerElement.innerHTML = `
+// mking components generic
+let innerHTML = '';
+items.forEach(item => {
+  innerHTML += `
   <div class="item-container">
-    <img class="item-image" src="${item.item_image}" alt="earring">
+    <img class="item-image" src="${item.image}" alt="earring">
 
-    <div class="rating">${item.rating.stars} ⭐ | ${item.rating.reviews}</div>
-    <div class="company-name">${item.company_name}</div>
+    <div class="rating">${item.rating.stars} ⭐ | ${item.rating.count}</div>
+    <div class="company-name">${item.company}</div>
     <div class="item-name">${item.item_name}</div>
 
     <div class="price">
       <span class="current-price">Rs ${item.current_price}</span>
       <span class="original-price">Rs ${item.original_price}</span>
-      <span class="discount">(${item.discount}% OFF)</span>
+      <span class="discount">(${item.discount_percentage}% OFF)</span>
     </div>
 
     <button class="btn-add-bag">Add to Bag</button>
-  </div>
-`;
+  </div>`
+})
+
+itemsContainerElement.innerHTML = innerHTML;
---
+++ Myntra_clone/scripts/items.js
@@ -0,0 +1,17 @@
+const items = [{
+    id:'001',
+    image: '1.jpg',
+    company: 'Carlton London',
+    item_name: 'Rhodium-Plated CZ Floral Studs',
+    original_price: 1045,
+    current_price: 606,
+    discount_percentage: 42,
+    rating: {
+        stars:4.5,
+        count: 1400,
+    },
+
+
+
+},
+]
